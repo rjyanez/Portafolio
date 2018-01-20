95f7f80e363caa78aaf44c0339acadcbf0d25fb4
rjyanez <reichelrei@gmail.com>
2018-01-19 23:12:09 -0400

    CAMBIOS GENERALES

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-// Efecto de Typeo header
+var navbar = document.getElementById("menu"),
+    sticky = navbar.offsetTop;
 
 var TxtType = function(el, toRotate, period) {
   this.toRotate = toRotate;
@@ -11,37 +12,29 @@ var TxtType = function(el, toRotate, period) {
 };
 
 TxtType.prototype.tick = function() {
-var i = this.loopNum % this.toRotate.length;
+  var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
-
   if (this.isDeleting) {
     this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
-
   this.el.innerHTML = '<span class="escritura">'+this.txt+'</span>';
-
   var that = this;
   var delta = 200 - Math.random() * 100;
-
   if (this.isDeleting) { delta /= 2; }
-
   if (!this.isDeleting && this.txt === fullTxt) {
   delta = this.period;
   this.isDeleting = true;
   } else if (this.isDeleting && this.txt === '') {
-  this.isDeleting = false;
-  this.loopNum++;
-  delta = 500;
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
   }
-
-  setTimeout(function() {
-  that.tick();
-  }, delta);
+  setTimeout(function() {that.tick();}, delta);
 };
 
-window.onload = function() {
+function typeText() {
   var elements = document.getElementsByClassName('tipeo');
   for (var i=0; i<elements.length; i++) {
       var toRotate = elements[i].getAttribute('data-type');
@@ -50,19 +43,9 @@ window.onload = function() {
         new TxtType(elements[i], JSON.parse(toRotate), period);
       }
   }
-
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".tipeo > .escritura { border-right: 1px solid #000}";
-  document.body.appendChild(css);
 };
 
-// Efecto sticky en el menu
-
-window.onscroll = function() {myFunction()};
-var navbar = document.getElementById("menu");
-var sticky = navbar.offsetTop;
-function myFunction() {
+function stickyMenu() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("menu-bottom");
     navbar.classList.remove("menu-top");
@@ -72,11 +55,39 @@ function myFunction() {
   }
 }
 
-// Efectos de las animaciones generales
-
-$( document ).ready(function() {
+function animated() {
   $(".animated").waypoint(function() {
     $(this).removeClass('hide');
     $(this).addClass($(this).data('animated'));
   }, { offset: '90%'});
-});
+};
+function openTimeline() {
+  $('.time-list > .time-item').click(function(){
+    $(".time-list > div.active").removeClass("active");
+    $(".timeline-content > div.show").removeClass("show");
+    $(this).addClass("active");
+    $(".timeline-content > div"+$(this).data('timeline')).addClass("show");    
+  });
+};
+function openSlider(){
+  var slideIndex = 1;
+  $('.slider > div.slider-button').click(function(){
+    if($(this).hasClass('slider-top')){
+      showSlider(slideIndex += -1,$(this).parent().attr('id')); 
+    }else{
+      showSlider(slideIndex += 1,$(this).parent().attr('id')); 
+    }
+  });
+  function showSlider(n,id) {
+    var x = $('#'+id+'.slider > div.slider-content');
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} 
+    for ( var i = 0; i < x.length; i++) {
+      $(x[i]).removeClass("show");      
+    }
+    $(x[slideIndex-1]).addClass("show"); 
+  };
+};
+
+$( document ).ready(function() {animated(),typeText(),openTimeline(),openSlider()});
+window.onscroll = function() {stickyMenu()};
